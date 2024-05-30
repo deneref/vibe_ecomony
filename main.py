b6a1af56a046e0b0251f3aef3815833121f51150
Даниил М <daniilmokeev13@gmail.com>
2024-05-30 13:43:08 +0300

    files moved

--- main.py
+++ main.py
@@ -21,21 +21,3 @@ totalSpending = sheetReader.readSheet_test(sn.totalSpending)
 # supply = sheetReader.readSheet(sn.supply)
 
 sheetWriter.writeToSheet(sn.result, totalSpending)
-
-'''
-# Пример записи в файл
-values = service.spreadsheets().values().batchUpdate(
-    spreadsheetId=spreadsheet_id,
-    body={
-        "valueInputOption": "USER_ENTERED",
-        "data": [
-            {"range": "B3:C4",
-             "majorDimension": "ROWS",
-             "values": [["This is B3", "This is C3"], ["This is B4", "This is C4"]]},
-            {"range": "D5:E6",
-             "majorDimension": "COLUMNS",
-             "values": [["This is D5", "This is D6"], ["This is E5", "=5+5"]]}
-  ]
-    }
-).execute()
-'''
